4bd58493243b292362401c80be60a970a26d8734
goodyoung <goodyoung.goh@gmail.com>
2024-02-10 17:43:24 +0900

    Update: change pkl file name

--- src/preprocess/country.py
+++ src/preprocess/country.py
@@ -2,28 +2,15 @@ import re
 
 
 class Country:
-    def __init__(self, **pkl):
-        self.area = {
-            "anguilla": "united kingdom",
-            "br": "none",
-            "a": "none",
-            "nd": "none",
-            "ny": "none",
-            "rj": "none",
-            "us": "united states",
-            "ca": "none",
-            "kerela": "india",
-            "pune": "india",
-            "colombia - cartagena": "colombia",
-            "country": "none",
-        }
-        self.pkl = pkl
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def country(self, df):
         """나라 컬럼 생성"""
 
         # 담당 자사 법인 맵핑
-        df["response_corporate2"] = df["response_corporate"].map(self.pkl)
+        df["response_corporate2"] = df["response_corporate"].map(self.response_corporate)
 
         df["customer_country"] = df["customer_country"].str.lower().str.strip()
         df["response_corporate2"] = df["response_corporate2"].str.lower().str.strip()
@@ -31,47 +18,25 @@ class Country:
         # 정규표현식으로 나라만 거르기
         df.loc[:, "country"] = df["customer_country"].str.extract(r"/([^/]+)$")[0]
         df["country"] = df["country"].str.strip()
-
-        # 숫자 포함 -> none으로 변경 및 결측치 채우기
-        df["country"] = df["country"].apply(
-            lambda x: "none" if re.search(r"\d", str(x)) else x
-        )
-        df["country"].fillna("none", axis=0, inplace=True)
-
-        # 이상한 수치 채우기
-        for i, j in self.area.items():
-            idx = df[df["country"] == i].index
-            if len(idx):
-                df.loc[idx, "country"] = j
-        # none 결측치 채우기
-        cond = df["country"] == "none"
-        df.loc[cond, "country"] = df[cond]["response_corporate2"]
+        
+        # country를 기준으로 채움
+        country_val = df['country'].values
+        country_unique = df['response_corporate2'].unique()
+        
+        for idx, country in enumerate(country_val):
+            if country not in country_unique: # 전처리 된 컬럼의 요소가 정해진 나라 범주에 없으면
+                df.loc[idx,'country'] = df.loc[idx,'response_corporate2'] # 해당 나라 범주로 채움
+        # 컬럼 드롭
+        df.drop(['response_corporate2'],axis = 1, inplace = True)
+        return df
+        
+    def continent(self,df):
+        """대륙 컬럼 생성"""
+        df['continent'] = df['country'].map(self.country_continent)
         return df
 
-    def city(self, df):
-        # 도시만 뽑기
-        df["city"] = df["customer_country"].str.split("/").str[-2]
-        df["city"] = df["city"].str.strip()
-
-        # others 처리
-        cond = df["city"] == ""
-        df.loc[cond, "city"] = "others"
-
-        # 결측치 채우기
-        df["city"].fillna("others", axis=0, inplace=True)
-
-        return df
-
-    def apply(self, df, module_list):
-        if not module_list:
-            raise ValueError("Not used modules")
-
-        if not isinstance(module_list, list):
-            module_list = [module_list]
-
-        for module in module_list:
-            method = getattr(self, module)
-
-            df = method(df)
-
+    def apply(self, df, module_list=None):
+        
+        df = self.country(df)
+        df = self.continent(df)
         return df
