9c35bb681e33a43e399e1bfb2837bd820142173d
EarthWon <kangjiwon5961@gmail.com>
2024-02-11 16:58:30 +0900

    modified customer

--- src/preprocess/customer.py
+++ src/preprocess/customer.py
@@ -1,5 +1,5 @@
+import pandas as pd
 import re
-
 
 class Customer:
     def __init__(self, **kwargs):
@@ -8,65 +8,73 @@ class Customer:
             setattr(self, key, value)
 
     def complementary(
-        self, customer_df, target_col, dictionary, comple_list, default_value="etc"
-    ):
-        """customer_column의 결측값을 채워주는 함수"""
-        customer_df.loc[:,["customer_type", "customer_job", "customer_position"]] = customer_df[["customer_type", "customer_job", "customer_position"]].applymap(
+        self, df:pd.DataFrame, target_col:list, derivated_col:str, dictionary:dict, comple_list:list, default_value="etc"
+    )-> pd.DataFrame:
+    
+        """ 결측값을 상호보완하여 채워주는 함수"""
+        
+        df[target_col] = df[target_col].applymap(
             lambda x: re.sub("(&|-|/|:)", "", x.lower().replace(" ", ""))
             if isinstance(x, str)
             else x
         )
-        customer_df[target_col] = None
-
+        df[derivated_col] = None
+    
         for col in comple_list:
             for key, value in dictionary.items():
                 for val in value:
                     true_list = []
                     val = re.sub("(&|-|/|:)", "", val).replace(" ", "").lower()
-
+    
                     true_list = list(
-                        customer_df.loc[
-                            customer_df[col].str.contains(val, na=False)
+                        df.loc[
+                            df[col].str.contains(val, na=False)
                         ].index
                     )
-                    customer_df.loc[
-                        (customer_df[target_col].isnull())
-                        & (customer_df.index.isin(true_list)),
-                        target_col,
+                    df.loc[
+                        (df[derivated_col].isnull())&(df.index.isin(true_list)), derivated_col,
                     ] = key
-
+    
         comple_col = comple_list[0]
-        customer_df.loc[
-            (customer_df[comple_col].notnull()) & (customer_df[target_col].isnull()),
-            target_col,
+        
+        df.loc[
+            (df[comple_col].notnull()) & (df[derivated_col].isnull()),
+            derivated_col,
         ] = "etc"
+    
+        # df[derivated_col].fillna("Unknown", inplace=True)
+        return df
 
-        customer_df[target_col].fillna("Unknown", inplace=True)
 
-        return customer_df
-
-    def apply(self, customer_df, pkl):
+    def apply(self, df, pkl):
         """실행 함수"""
         customer_type2 = self.customer_type2
         job_function = self.job_function
         seniority_level = self.seniority_level
 
-        customer_df = self.complementary(
-            customer_df,
+        customer_col=["customer_type", "customer_job", "customer_position"]
+
+        df = self.complementary(
+            df,
+            customer_col,
             "customer_type2",
             customer_type2,
             ["customer_type", "customer_job", "customer_position"],
         )
-        customer_df = self.complementary(
-            customer_df,
+        df = self.complementary(
+            df,
+            customer_col,
             "job_function",
             job_function,
             ["customer_job", "customer_type", "customer_position"],
         )
-        customer_df = self.complementary(
-            customer_df,
+        df = self.complementary(
+            df,
+            customer_col,
             "seniority_level",
             seniority_level,
             ["customer_position", "customer_type", "customer_job"],
         )
-        return customer_df
+        return df
+
+  
