4a6f06837dfa7e3c72bce89c15f0ac183b476a14
goodyoung <goodyoung.goh@gmail.com>
2024-02-12 22:27:06 +0900

    Update: customer idx converted ratio by lead owner

--- src/preprocess/convert_ratio.py
+++ src/preprocess/convert_ratio.py
@@ -5,7 +5,7 @@ class Convert_ratio:
             setattr(self, key, value)
             
     def make_ratio_to_dict(self,df,col_name):
-        """비율 딕셔너리 저장"""
+        """ 전환율 딕셔너리 저장 """
         # from phase2_module.src.utils.manage_pkl_files import PickleManager as pkl_manager
         dict_ ={}
         flag = df.groupby(col_name)['is_converted'].agg(['sum','count'])
@@ -17,9 +17,20 @@ class Convert_ratio:
         # save
         # pkl_manager.save(f"{col_name}",f"{col_name}_converted_ratio",dict_)
         return dict_
+        
+    def lead_owner_customer_idx_mean(self,df):
+        """ 실적 별 회사 전환율 평균 """
+        # 범주화
+        bin = [0.0001,10.0001,20.0001,30.0001,40.0001,50.0001,60.0001,70.0001,80.0001,90.0001,100.00,100.001]
+        df.loc[:,'customer_idx_lead_owner_mean'] = pd.cut(df['lead_owner_converted_ratio'], bins=bin, right = False,labels= False)
+        df['lead_owner_customer_idx_mean'] = df['customer_idx_lead_owner_mean']+1
+        df['lead_owner_customer_idx_mean'].fillna(0.0,axis = 0, inplace  = True)
+        # 매핑
+        df['lead_owner_customer_idx_mean'].map(self.lead_owner_customer_idx_mean)
+        return df    
 
     def ratio_preprocessing(self,df, col_name, ratio_dict):
-        # 검증
+        """ 해당 컬럼 전환율 변환 """
         for i in np.sort(df[col_name].unique()):
             if i not in ratio_dict.keys(): # 없으면 추가
                 self.lead_owner_converted_ratio[i] = 0.0
@@ -28,6 +39,9 @@ class Convert_ratio:
         
     def lead_owner(self,df):
         return self.ratio_preprocessing(df, 'lead_owner', self.lead_owner_converted_ratio)
+        
+    def customer_idx(self,df):
+        return self.ratio_preprocessing(df, 'customer_idx', self.customer_idx_converted_ratio)
     
     def apply(self, df, module_list):
         if not module_list:
