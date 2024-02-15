becd2ad3cb0d10302faff427ffba61db36434942
goodyoung <goodyoung.goh@gmail.com>
2024-02-15 15:51:19 +0900

    Update: customer_idx categorization

--- src/preprocess/basic.py
+++ src/preprocess/basic.py
@@ -9,7 +9,14 @@ class Basic:
         df = df.drop_duplicates()
         df.reset_index(drop=True, inplace=True)
         return df
-
+        
+    def cusotmer_idx_categorization(self,df, bins=[0, 2, 10, 100, 1000, 10000]):
+        """customer_idx 범주화"""
+        customer_idx_counts = df['customer_idx'].value_counts()
+        df_counts = pd.cut(customer_idx_counts, bins=bins, labels=False, right=False)
+        df['customer_idx_group'] = df['customer_idx'].map(df_counts)
+        return df
+        
     def apply(self, df, module_list):
         if not module_list:
             raise ValueError("Not used modules")
