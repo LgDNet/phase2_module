fd742b5b5cb16638297c9ac79cd41dd79c7704da
Seungbeom oh <osr0912@gmail.com>
2024-02-15 20:17:27 +0900

    fit: product_cate fill_odds

--- src/preprocess/product_category.py
+++ src/preprocess/product_category.py
@@ -74,16 +74,11 @@ class ProductCategory:
         df = df[df["product_category"] != "washing machine,dryer"]
         
         # product_category
-        data = df[['product_category','product_subcategory', 'product_modelname','customer_interest']].copy()
+        data = df[['business_unit','product_category','product_subcategory', 'product_modelname','customer_interest']].copy()
         data["product_category"] = data["product_category"].replace(self.replacement_dict).str.replace("solar,", "")
         data["mapped"] = data["product_category"].apply(
             lambda x: next((v for k, v in self.filter1.items() if k in x), x)
         )
-        category_counts = data["mapped"].value_counts()
-        categories_to_replace = category_counts[
-            category_counts < 6
-        ].index.tolist()  # 6개 미만 index 찾기
-        data['mapped'] = data['mapped'].apply(lambda x: 'others' if x in categories_to_replace else x)
         data["category_1"] = data["mapped"].map(self.cate_dict)
         data["category_1"] = data["category_1"].map(self.cate_num_dict)
         data["category_2"] = data["mapped"].map(self.subcate_dict)
@@ -91,11 +86,11 @@ class ProductCategory:
         
         # 8번 진행.
         cond8 = (data['customer_interest'] == 8)
-        data.loc[cond8, ['category_1','category_2', 'category_3']] = data.loc[cond8, ['category_1', 'category_2', 'category_3']].fillna('space')
-        data["cate_is_nan_all"] = (data[["category_1", "category_2", "category_3"]].isna().any(axis=1)) | (data[["category_1", "category_2", "category_3"]].apply(lambda row: 'all' in row.values, axis=1))
-        
+        data.loc[cond8, ['category_1','category_2', 'category_3']] = data.loc[cond8, ['category_1', 'category_2', 'category_3']].fillna('Space')
+        data["cate_is_nan"] = (data[["category_1", "category_2", "category_3"]].isna().any(axis=1))
+       
         # 1, 2번, 4번, 6번 진행
-        cond4 = (((data['customer_interest'] == 1) | (data['customer_interest'] ==2) | (data['customer_interest'] == 4) | (data['customer_interest'] == 6)) & data['cate_is_nan_all'] == True)
+        cond4 = (((data['customer_interest'] == 1) | (data['customer_interest'] ==2) | (data['customer_interest'] == 4) | (data['customer_interest'] == 6)) & data['cate_is_nan'] == True)
         data.loc[cond4, "mapped"] = data.loc[cond4, "product_subcategory"].replace(self.six_replace_dict)
         data.loc[cond4, "mapped"] = data.loc[cond4, "mapped"].apply(
             lambda x: next((v for k, v in self.six_contain_dict.items() if k in x), x)
@@ -104,10 +99,10 @@ class ProductCategory:
         data.loc[cond4, "category_1"] = data.loc[cond4, "mapped"].map(sub_cate_dict)
         data.loc[cond4, "category_2"] = data.loc[cond4, "mapped"].map(sub_subcate_dict)
         data.loc[cond4, "category_3"] = data.loc[cond4, "mapped"].map(sub_subsubcate_dict)
-        data["cate_is_nan_all"] = (data[["category_1", "category_2", "category_3"]].isna().any(axis=1)) | (data[["category_1", "category_2", "category_3"]].apply(lambda row: 'all' in row.values, axis=1))
+        data["cate_is_nan"] = (data[["category_1", "category_2", "category_3"]].isna().any(axis=1))
         
         # 1, 2, 3, 5 진행.
-        cond3 = (((data['customer_interest'] == 1) | (data['customer_interest'] == 2)| (data['customer_interest'] == 3) | (data['customer_interest'] == 5))& data['cate_is_nan_all'] == True)
+        cond3 = (((data['customer_interest'] == 1) | (data['customer_interest'] == 2)| (data['customer_interest'] == 3) | (data['customer_interest'] == 5))& data['cate_is_nan'] == True)
         data.loc[cond3, "mapped"] = data.loc[cond3, "product_modelname"].apply(
             lambda x: next((v for k, v in self.model_contain_dict.items() if k in x), x)
         )
@@ -115,7 +110,21 @@ class ProductCategory:
         data.loc[cond3, "category_2"] = data.loc[cond3, "mapped"].map(sub_subcate_dict)
         data.loc[cond3, "category_3"] = data.loc[cond3, "mapped"].map(sub_subsubcate_dict)
         
+        # 예외처리.
+        data["cate_is_nan"] = (data[["category_1", "category_2", "category_3"]].isna().any(axis=1))
+        cond1 = data["cate_is_nan"] == True
+        data.loc[cond1, ['category_1','category_2', 'category_3']] = data.loc[cond1, ['category_1', 'category_2', 'category_3']].fillna('unknown')
+        
         # reset_index 진행.
         df[['category_1', 'category_2', 'category_3']] = data[['category_1', 'category_2', 'category_3']]
+        df.reset_index(drop = True, inplace = True)
+        return df
+    
+    def fill_odds(self, df):
+        mapping_dict = {'ID': 'commercial_display', 'AS': 'hvac', 'IT': 'it_products'}
+        mask = df['category_1'].isin(['Space', 'others', 'unknown']) # 만약에 이상치라면,
+        df.loc[mask, 'category_1'] = df.loc[mask, 'business_unit'].map(mapping_dict)
+        df.loc[mask & df['category_1'].isin(['commercial_display', 'hvac', 'it_products']), ['category_2', 'category_3']] = 'all'
+        df = df.dropna(subset=['category_1'])
         df.reset_index(drop = True, inplace = True)
         return df
