a07b719d93653a9cfeb86510033419c573d2ebae
Seungbeom oh <osr0912@gmail.com>
2024-02-21 19:13:02 +0900

    update: customer_type

--- src/preprocess/customer2.py
+++ src/preprocess/customer2.py
@@ -45,30 +45,38 @@ class Customer2:
                     for key in self.job_dict:
                         if key in row[col]:
                             return self.job_dict[key]
-                        elif row[col] =='Space':
-                            return 'Space'
+            for col in columns:
+                if isinstance(row[col], str) and 'Space' in row[col]:
+                    return 'Space'
             return 'others'
         
-        data['customer_job'] = data.apply(map_job, axis =1)
-        df['job_function'] = data['customer_job']
+        data['job_function'] = data.apply(map_job, axis =1)
+        df[['job_function', 'customer_job']] = data[['job_function', 'customer_job']]
         return df
         
     def customer_type(self, df):
         data =df[['customer_job','customer_type','customer_position','customer_type2','seniority_level','job_function']].copy()
         data['customer_type'] =data['customer_type'].fillna('Space')
         def map_type(row):
-            columns = ['customer_type', 'customer_type2','customer_position', 'customer_job']
+            columns = ['customer_type', 'customer_type2','customer_position', 'customer_job', 'job_function']
             for col in columns:
                 if isinstance(row[col], str):
                     for key in self.customer_type_filter:
                         if key in row[col]:
                             return self.customer_type_filter[key]
+            for col in columns:
+                if isinstance(row[col], str) and 'other' in row[col]:
+                    return 'others'
             return row['customer_type']
         
         data['customer_type'] = data.apply(map_type, axis =1)
         customer_type1, customer_type2 = self.get_type_dict(self.customer_type_dict)
-        data['new_customer_type1'] = data['customer_type'].map(customer_type1)
-        data['new_customer_type2'] = data['customer_type'].map(customer_type2)
+        data["new_customer_type1"] = data["customer_type"].apply(
+                lambda x: next((v for k, v in customer_type1.items() if k in x), 'others')
+            )
+        data["new_customer_type2"] = data["customer_type"].apply(
+                lambda x: next((v for k, v in customer_type2.items() if k in x), 'others')
+         )
         df[['new_customer_type1','new_customer_type2']] = data[['new_customer_type1','new_customer_type2']]
         return df
         
